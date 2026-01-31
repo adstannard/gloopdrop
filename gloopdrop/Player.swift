775cbd560d147e4c713cb90bf3135cd7eae7cce3
Andrew Stannard <andrew.stannard@gmail.com>
2026-01-31 20:58:38 +0000

    GloopDrop
    Game Tutorial in SpriteKit

---
+++ gloopdrop/Player.swift
@@ -0,0 +1,60 @@
+//
+//  Player.swift
+//  gloopdrop
+//
+//  Created by Andrew Stannard on 31/01/2026.
+//
+
+import Foundation
+import SpriteKit
+
+// This emun lets you easily switch between animations
+enum PlayerAnimationType: String {
+    case walk
+}
+
+class Player: SKSpriteNode {
+    // MARK: - PROPERTIES
+    
+    //Textures (Animation)
+    private var walkTextures: [SKTexture]?
+    
+    // MARK: - INIT
+    
+    init() {
+        // Set default texture
+        let texture = SKTexture(imageNamed: "blob-walk_0")
+        
+        // Call to super.init
+        super.init(texture: texture, color: .clear, size: texture.size())
+        
+        // Set up animation textures
+        self.walkTextures = self.loadTextures(atlas: "blob", prefix: "blob-walk_",
+                                              startsAt: 0, stopsAt: 2)
+        
+        // Set up other properties after init
+        self.name = "player"
+        self.setScale(1.0)
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.0) // centre-bottom
+        self.zPosition = Layer.player.rawValue
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - METHODS
+    
+    func walk() {
+        // Check for textures
+        guard let walkTextures = walkTextures else {
+            preconditionFailure("Could not find textures!")
+        }
+        // Run animation (forever)
+        startAnimation(textures: walkTextures, speed: 0.25,
+                       name: PlayerAnimationType.walk.rawValue,
+                       count: 0, resize: true, restore: true)
+    }
+    
+    
+}
